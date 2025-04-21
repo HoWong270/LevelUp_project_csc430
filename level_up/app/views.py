1a145e2a9e87e5a3f720a099fb2b4cb07435c8dd
Egor Gusev <egor.gusev2018@gmail.com>
2025-04-21 12:34:53 -0400

    changed from function based view, to class based view.

--- level_up/app/views.py
+++ level_up/app/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from app.models import Article
-from app.forms import CreateArticleFrom
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "app/home.html", {"articles":articles})
 
-def create_article(request):
-    if request.method == "POST":
-        #data is submitted
-        form = CreateArticleFrom(request.POST)
-        if form.is_valid():
-            form_data = form.cleaned_data
-            new_article = Article (
-                title = form_data['title'],
-                status = form_data['status'],
-                content = form_data['content'],
-                word_count = form_data['word_count'],
-                twitter_post = form_data['twitter_post']
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
 
-            )
-            new_article.save()
-    else:
-        form = CreateArticleFrom()
-    return render(request, "app/article_create.html", {"form": form})
-    
+
+class ArticleCreateView(CreateView):
+    template_name = "app/article_create.html"
+    model = Article
+    fields = ["title","status","content","word_count","twitter_post"]
+    success_url = reverse_lazy("home")
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title","status","content","word_count","twitter_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "articles"
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home")
+
+
 
 
