6e512c6dc1bfaa1dd781a6a37e92f479bae81d79
Egor Gusev <egor.gusev2018@gmail.com>
2025-04-22 23:12:11 -0400

    added verification for articles

--- level_up/app/migrations/0002_create_superuser.py
+++ level_up/app/migrations/0002_create_superuser.py
@@ -2,23 +2,7 @@
 from django.utils import timezone
 from django.db import migrations
 
-def create_superuser(apps, schema_editor):
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
-
-    if User.objects.exists():
-        return
-    
-    superuser = User.objects.create_superuser(
-        username="example_username",
-        email="example@example.com",
-        password="example_password",
-        last_login=timezone.now()
-    )
-
-    superuser.save()
-    
+ 
 
 
 class Migration(migrations.Migration):
@@ -26,6 +10,23 @@ class Migration(migrations.Migration):
     dependencies = [
         ('app', '0001_initial'),
     ]
+
+    def create_superuser(apps, schema_editor):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if User.objects.exists():
+            return
+    
+        superuser = User.objects.create_superuser(
+            username="example_username",
+            email="example@example.com",
+            password="example_password",
+            last_login=timezone.now()
+    )
+
+        superuser.save()
 
     operations = [
         migrations.RunPython(create_superuser)
--- level_up/app/views.py
+++ level_up/app/views.py
@@ -1,21 +1,23 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from app.models import Article
-from django.views.generic import (
-    CreateView,
-    ListView,
-    UpdateView,
-    DeleteView
-)
+from django.views.generic import CreateView,ListView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
-
-class ArticleListView(ListView):
+#LoginRequireMixin verifies user must be logged in, in order to see, create
+#and delete articles. get_queryset function allows the users to view articles, that they created
+class ArticleListView(LoginRequiredMixin,ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
 
+    def get_queryset(self) -> QuerySet[Any]:
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title","status","content","twitter_post"]
@@ -26,20 +28,28 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
     
 
-
-class ArticleUpdateView(UpdateView):
+#UserPassesTextMixin, overrides test in order to access this page 
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     template_name = "app/article_update.html"
     model = Article
     fields = ["title","status","content","twitter_post"]
     success_url = reverse_lazy("home")
     context_object_name = "articles"
 
+    #verfies that logged in user, is the creator of the object
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     template_name = "app/article_delete.html"
     model = Article
     success_url = reverse_lazy("home")
     context_object_name = "articles"
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
+
 
 
 
