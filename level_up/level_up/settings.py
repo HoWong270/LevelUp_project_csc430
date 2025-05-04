bdc433a2d356cbe542384d2b82ee5642deebd771
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-04 09:51:24 -0400

    updated the social account sign in, added github allauth

--- level_up/level_up/settings.py
+++ level_up/level_up/settings.py
@@ -157,8 +157,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = ""
+LOGOUT_REDIRECT_URL = "home"
 ACCOUNTS_LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 #Allauth settings
