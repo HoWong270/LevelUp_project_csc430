37520b3d967481413c662d91e8eaac052235de10
Egor Gusev <egor.gusev2018@gmail.com>
2025-05-06 00:39:32 -0400

    added last styling edits

--- level_up/level_up/urls.py
+++ level_up/level_up/urls.py
@@ -19,12 +19,15 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 from django.urls import reverse_lazy
 
+from allauth.account.views import SignupView
+
 urlpatterns = [
 
-    path("", RedirectView.as_view(url=reverse_lazy("account_login"))),
-    path("accounts/",include("allauth.urls")),
     path("admin/", admin.site.urls),
-    path("articles/",include("app.urls")), 
+    path("articles/",include("app.urls")),
+    path("", SignupView.as_view(),name="account_signup"),
+    path("accounts/signup/",RedirectView.as_view(url="/")),
+    path("accounts/",include("allauth.urls")), 
     path("__debug__/",include("debug_toolbar.urls")),
     path("__reload__/",include("django_browser_reload.urls")),
 ]
